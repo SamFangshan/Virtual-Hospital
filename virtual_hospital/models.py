cebd9ab4ffe8ecea2bf1de877b4e77a4fa48f12b
Jiang Fangshan <fjiang003@e.ntu.edu.sg>
2020-09-13 13:38:19 +0800

    prescription-drug relationship

--- virtual_hospital/models.py
+++ virtual_hospital/models.py
@@ -1,6 +1,8 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask_login import UserMixin
 from virtual_hospital import db
 
 
@@ -8,7 +10,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128))
+    email = db.Column(db.String(128), unique=True)
     phone_number = db.Column(db.String(20))
     nric = db.Column(db.String(10))
     gender = db.Column(db.String(6))
@@ -47,7 +49,7 @@ class Doctor(User):
     specialties = db.Column(db.ARRAY(db.String(100)))
     office_hour_start_time = db.Column(db.String(10))
     office_hour_end_time = db.Column(db.String(10))
-    deparment_id = db.Column(db.Integer, db.ForeignKey(Department.id), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey(Department.id), nullable=True)
     appointment_time_slots = db.relationship('AppointmentTimeSlot', lazy='select',
                                              backref=db.backref('doctor', lazy='joined'))
 
@@ -79,7 +81,27 @@ class Prescription(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     patient_id = db.Column(db.Integer, db.ForeignKey(Patient.id), nullable=False)
     doctor_id = db.Column(db.Integer, db.ForeignKey(Doctor.id), nullable=False)
+
     pick_up_start_date = db.Column(db.Date, nullable=False)
     pick_up_status = db.Column(db.String(10), nullable=False)
     medicines = db.Column(db.ARRAY(db.String(100)))
     prescription_instructions = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    drugs = db.relationship('Drug', secondary='prescription_drugs')
+
+
+class Drug(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+
+
+class Prescription_Drug(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    prescription_id = db.Column(db.Integer, db.ForeignKey(Prescription.id), nullable=False)
+    drug_id = db.Column(db.Integer, db.ForeignKey(Drug.id), nullable=False)
+
+    prescription = db.relationship(Prescription, backref=db.backref('prescription_drugs'))
+    drug = db.relationship(Drug, backref=db.backref('prescription_drugs'))
