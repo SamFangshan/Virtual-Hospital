595dbaab7b478e7560812ad7d5a34248b4ec38ab
ianpoar <ianpoar@hotmail.com>
2020-09-15 15:20:56 +0800

    Test form to test communication with db (rerun requirements.txt)
    localhost:5000/test
    
    example existing email to test: limlingchoo57@hospital.com

--- virtual_hospital/views.py
+++ virtual_hospital/views.py
@@ -1,11 +1,18 @@
-from flask import render_template
+from flask import Flask, render_template
 from virtual_hospital import app
-
+from virtual_hospital.models import *
+from virtual_hospital.forms import *
 
 @app.route('/')
 def index():
     return render_template('index.html', currPage="Home")
-    
+
 @app.route('/about')
 def about():
     return render_template('about.html', currPage="About")
+
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    test_form = TestForm()
+    test_form.validate_on_submit()
+    return render_template("test.html", form=test_form)
