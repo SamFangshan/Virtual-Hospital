63d74325bf18b71892af48cef3f044dee7072639
AlaTan93 <tans0380@e.ntu.edu.sg>
2020-10-29 19:51:59 +0800

    test appointments script complete

--- tests/test_appointments.py
+++ tests/test_appointments.py
@@ -3,7 +3,6 @@ from virtual_hospital.models import *
 from datetime import date
 
 def test_appointments():
-    # wip
 
     client = app.test_client()
     client.post('/login', data=dict(
@@ -20,6 +19,7 @@ def test_appointments():
     # doctor client
 
     user = User.query.filter_by(email="alatest2@test.com").first()
+    user2 = User.query.filter_by(email="alatest1@test.com").first()
     searchString = '/profile?id=' + user.id
     
     response1 = client.get(searchString)
@@ -34,9 +34,28 @@ def test_appointments():
     today = date.today()
     d1 = today.strftime("%Y-%m-%d")
     assert d1 in data # checks for if today's date is properly in webpage
+    time_slot_data = AppointmentTimeSlot.query.filter_by(doctor_id=doctor_id).first()
     
-    client.post('/newappointment', data=dict(), follow_redirects=True)
+    response2 = client.post('/newappointment', data=dict(
+            doctor_id=user.id,
+            appointment_time_slot_id=time_slot_data.id         
+        ), follow_redirects=True)
     # new appointment stuff
+
+    assert response2.status_code == 200
+    assert b'Appointment booked.' in response2.get_data(as_text = True) # checks if appointment has been booked by client
+      
+    response3 = client2.get('appointments')
+    assert response3.status_code == 200    
+
+    data = response3.get_data(as_text = True)
+    fullString = user2.name + ' (Queue: '
+    assert fullString in data
+
+    
+ 
+
+
 
     
     
